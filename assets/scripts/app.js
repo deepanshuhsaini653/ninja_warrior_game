a09a3f00948e022f683db75977443b064b5021e6
Deepak Saini <deepak.saini@netprophetsglobal.com>
2021-05-25 18:39:39 +0530

    d

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -12,8 +12,8 @@ $('.start_now').click(function () {
 var player = {
     position: 0,
 }
-// const base_Url = window.location.origin + '/ninja_warrior';
-const base_Url = window.location.origin + '/';
+const base_Url = window.location.origin + '/ninja_warrior';
+// const base_Url = window.location.origin + '/';
 String.prototype.replaceAt = function (index, replacement) {
     return this.substr(0, index) + replacement + this.substr(index + replacement.length);
 }
